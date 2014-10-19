8d60e55a875426b3aa1ed1a4dc374c1f47229d8c
edwardshen123 <edwardshen123@gmail.com>
2014-10-19 08:52:21 -0400

    Added to Stuyablo, and World

--- rpg/Version0.0.2/World.java
+++ rpg/Version0.0.2/World.java
@@ -1,10 +1,15 @@
 public class World {
     
     private String currentLocation;
+    private Boolean isTown;
+    private String[] locationTowns = {"Freshyville"};
     
-    public world() {
+    public World() {
+	currentLocation = "Freshyville";
+	isTown = true;
     }
-    public world(String lastLocation) {
+    public World(String lastLocation) {
+	currentLocation = lastLocation;
     }
 
     public String getLocation() {
@@ -13,5 +18,12 @@ public class World {
     public String move(String nextLocation) {
 	currentLocation = nextLocation;
 	return ("You have traveled to " + currentLocation);
+    }
+    public String[] actionOptions() {
+	if (isTown) {
+	    return (new String[] {"Armor\'s Shop", "Nurse\'s Office"});
+	} else {
+	    return (new String[] {"Search", "Camp"});
+	}
     }
 }
